673a23884910c9e0e4b1a6a88e04242598343532
alimukati <alimukati@gmail.com>
2025-08-31 15:45:16 +0500

    Add files via upload

---
+++ stylenew.css
@@ -0,0 +1,79 @@
+*{
+    margin: 0px;
+    padding: 0px;
+}
+body{
+    background-color: rgb(10,25,47);
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+nav{
+    width: 100%;
+    height: 90px;
+    background-color: gray;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+.uper-logo{
+    width: 400px;
+    height: 90px;
+    /* background-color: red; */
+    display: flex;
+    flex-direction: row;
+    justify-content: end;
+    align-items: center;
+}
+.logo{
+    width: 300px;
+    height: 80px;
+    /* background-color: yellow; */
+    display: flex;
+    flex-direction: row;
+    justify-content:space-evenly;
+    align-items: center;
+    color: white;
+    font-size: 40px;
+}
+
+.uper-text{
+    width: 700px;
+    height: 90px;
+    /* background-color: red; */
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+}
+.text{
+    width: 500px;
+    height: 80px;
+    /*background-color: yellow;*/
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+}
+.small-header-col{
+    width:50%;
+    float:left;
+}
+.social-icons{
+    text-align: right;
+}
+.social-icons span,.small-header-contect{
+    display: inline-block;
+    
+}
+.social-icons img{
+    max-width: 28px;
+}
+.footer{ 
+        width:100%;
+        height:auto;
+        overflow: hidden;
+     background-color: #333333 ;
+     color:white;
+     margin-top: 250px;
+    padding: 50px 0;
+    }
